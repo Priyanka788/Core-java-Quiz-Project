c64f2d40d2afcf9220a626d21c13edd1bcbbfb3b
Priyanka788 <51821861+Priyanka788@users.noreply.github.com>
2019-11-22 06:08:33 +0530

    Fetch database ques with correct option

---
+++ Resultdisplay.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication4;
+
+/**
+ *
+ * @author DELL
+ */
+public class ResultDisplay {
+     private String Question;
+      private  String Correct;
+      private  String Choose;
+    public String getQuestion()
+    {
+        return Question;
+    }
+    
+    public void setQuestion(String Question)
+           
+    {
+      this.Question=Question;  
+    }
+     public String getCorrect()
+    {
+        return Correct;
+    }
+    
+    public void setCorrect(String Correct)
+           
+    {
+      this.Correct=Correct;  
+    }
+    public String getChoose()
+    {
+        return Choose;
+    }
+    
+    public void setChoose(String Choose)
+           
+    {
+      this.Choose=Choose;  
+    } 
+    
+}
