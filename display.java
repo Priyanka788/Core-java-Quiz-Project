cf752499b68c3408559992020fd2edba0321b17c
Priyanka788 <51821861+Priyanka788@users.noreply.github.com>
2019-11-22 06:05:31 +0530

    Display ques code in jframe

---
+++ display.java
@@ -0,0 +1,83 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication4;
+
+/**
+ *
+ * @author DELL
+ */
+public class Display {
+    
+    private String Question;
+    private String Option1;
+    private String Option2; 
+    private String Option3;
+    private String Option4;
+    private  String Correct;
+    public String getQuestion()
+    {
+        return Question;
+    }
+    
+    public void setQuestion(String Question)
+           
+    {
+      this.Question=Question;  
+    }
+    
+     public String getOption1()
+    {
+        return Option1;
+    }
+    
+    public void setOption1(String Option1)
+           
+    {
+      this.Option1=Option1;  
+    }
+     public String getOption2()
+    {
+        return Option2;
+    }
+    
+    public void setOption2(String Option2)
+           
+    {
+     this.Option2=Option2;   
+    }
+     public String getOption3()
+    {
+        return Option3;
+    }
+    
+    public void setOption3(String Option3)
+           
+    {
+      this.Option3=Option3;  
+    }
+     public String getOption4()
+    {
+        return Option4;
+    }
+    
+    public void setOption4(String Option4)
+           
+    {
+      this.Option4=Option4;  
+    }
+    
+     public String getCorrect()
+    {
+        return Correct;
+    }
+    
+    public void setCorrect(String Correct)
+           
+    {
+      this.Correct=Correct;  
+    }
+    
+}
